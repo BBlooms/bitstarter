2ec1ad7d73001475fd0c8fee306a950dcd4aec58
BBlooms <bloomer.brian@gmail.com>
2013-07-23 21:51:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
+var buffer = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html').toString('utf-8'));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
